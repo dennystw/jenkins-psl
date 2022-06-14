d2a0c101effb77a646a0d364c4a20ccb96ff3a70
Alfian Firmansyah <alfianvansykes@gmail.com>
2022-06-14 11:38:30 +0700

    Update Pipeline.groovy

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -1,7 +1,5 @@
 package com.workshop
 
-import com.workshop.Config
-
 class Pipeline {
     String repository_name
     String branch_name
@@ -11,7 +9,6 @@ class Pipeline {
     String pr_num
     String dockerTool
 
-    // Set pipeline struct / constructor / test jenkins job asdad aaaa
     Pipeline(String repository_name, String branch_name, String git_user, String docker_user, String app_port, String pr_num,
        String dockerTool){
        this.repository_name = repository_name
